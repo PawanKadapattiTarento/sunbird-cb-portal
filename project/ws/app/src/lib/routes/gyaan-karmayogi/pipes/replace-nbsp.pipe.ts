43d5bb9e5cf42daf3c63ff82319b236d34f2836c
nitinraj-tarento <nitin.raj@tarento.com>
2024-09-13 14:29:10 +0530

    lint fix

--- project/ws/app/src/lib/routes/gyaan-karmayogi/pipes/replace-nbsp.pipe.ts
+++ project/ws/app/src/lib/routes/gyaan-karmayogi/pipes/replace-nbsp.pipe.ts
@@ -1,7 +1,7 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core'
 
 @Pipe({
-  name: 'replaceNbsp'
+  name: 'replaceNbsp',
 })
 export class ReplaceNbspPipe implements PipeTransform {
 
